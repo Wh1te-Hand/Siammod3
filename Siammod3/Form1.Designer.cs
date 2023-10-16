ce568a5f4ee8b76a4e2d7fcb8ce82827b4d6d717
White Hand <White Hand@DESKTOP-8O2Q7AJ>
2023-10-16 04:09:41 +0300

    Добавьте файлы проекта.

---
+++ Siammod3/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Siammod3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_p1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox_p2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox_numberTacts = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button_calculate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_p1
+            // 
+            this.textBox_p1.Location = new System.Drawing.Point(88, 21);
+            this.textBox_p1.Name = "textBox_p1";
+            this.textBox_p1.Size = new System.Drawing.Size(100, 22);
+            this.textBox_p1.TabIndex = 0;
+            this.textBox_p1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_p1_KeyPress);
+            this.textBox_p1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox_p1_KeyUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox_p2
+            // 
+            this.textBox_p2.Location = new System.Drawing.Point(88, 63);
+            this.textBox_p2.Name = "textBox_p2";
+            this.textBox_p2.Size = new System.Drawing.Size(100, 22);
+            this.textBox_p2.TabIndex = 3;
+            this.textBox_p2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_p2_KeyPress);
+            this.textBox_p2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox_p2_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "p1=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "p2=";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button_calculate);
+            this.groupBox1.Controls.Add(this.textBox_numberTacts);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_p1);
+            this.groupBox1.Controls.Add(this.textBox_p2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(827, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(242, 197);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Probability";
+            // 
+            // textBox_numberTacts
+            // 
+            this.textBox_numberTacts.Location = new System.Drawing.Point(88, 103);
+            this.textBox_numberTacts.Name = "textBox_numberTacts";
+            this.textBox_numberTacts.Size = new System.Drawing.Size(100, 22);
+            this.textBox_numberTacts.TabIndex = 6;
+            this.textBox_numberTacts.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_numberTacts_KeyPress);
+            this.textBox_numberTacts.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox_numberTacts_KeyUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Num of tact=";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(437, 381);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Analytical variables";
+            // 
+            // button_calculate
+            // 
+            this.button_calculate.Location = new System.Drawing.Point(154, 156);
+            this.button_calculate.Name = "button_calculate";
+            this.button_calculate.Size = new System.Drawing.Size(75, 23);
+            this.button_calculate.TabIndex = 8;
+            this.button_calculate.Text = "calculate";
+            this.button_calculate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_p1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox_p2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_numberTacts;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_calculate;
+    }
+}
+
---
+++ Siammod3/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Siammod3
+{
+    public partial class Form1 : Form
+    {
+        private long p1, p2, number_tacts;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_p1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            String line = "";
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_p2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            String line = "";
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_p2_KeyUp(object sender, KeyEventArgs e)
+        {
+            long N_number;
+            String line = "";
+            line = this.textBox_p2.Text; //changed
+            if (line != "")
+            {
+                N_number = long.Parse(line);
+                if (N_number > 0 && N_number != double.NaN)
+                {
+                    p2 = N_number;//
+                }
+            }
+            else
+            {
+                p2 = 0; //
+            }
+        }
+
+        private void textBox_numberTacts_KeyUp(object sender, KeyEventArgs e)
+        {
+            long N_number;
+            String line = "";
+            line = this.textBox_numberTacts.Text; //changed
+            if (line != "")
+            {
+                N_number = long.Parse(line);
+                if (N_number > 0 && N_number != double.NaN)
+                {
+                    number_tacts = N_number;//
+                }
+            }
+            else
+            {
+                number_tacts = 0; //
+            }
+        }
+
+        private void textBox_numberTacts_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            String line = "";
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_p1_KeyUp(object sender, KeyEventArgs e)
+        {
+            long N_number;
+            String line = "";
+            line = this.textBox_p1.Text; //changed
+            if (line != "")
+            {
+                N_number = long.Parse(line);
+                if (N_number > 0 && N_number != double.NaN)
+                {
+                    p1 = N_number;//
+                }
+            }
+            else
+            {
+               p1=0; //
+            }
+        }
+    }
+}
